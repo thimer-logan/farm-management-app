c1f7183801bc6c8c607cde72f7507dee99bcec00
Logan Thimer <thimerlogan@gmail.com>
2025-02-17 09:54:16 -0700

    Installed react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 );
