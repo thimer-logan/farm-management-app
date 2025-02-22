6fa3f883163b8642fc9a7eb92ae5388cca30009f
Logan Thimer <thimerlogan@gmail.com>
2025-02-22 14:18:19 -0700

    Backend - Added identity tables to database.

--- backend/PrecisionFarming.Api/Program.cs
+++ backend/PrecisionFarming.Api/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrecisionFarming.Domain.Entities.Identity;
+using PrecisionFarming.Infrastructure.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<AppUser, AppRole>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<AppDbContext>()
+  .AddDefaultTokenProviders()
+  .AddUserStore<UserStore<AppUser, AppRole, AppDbContext, Guid>>()
+  .AddRoleStore<RoleStore<AppRole, AppDbContext, Guid>>();
 
 var app = builder.Build();
 
@@ -16,8 +39,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHsts();
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+app.UseRouting();
+app.UseCors();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ backend/PrecisionFarming.Infrastructure/DbContext/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrecisionFarming.Domain.Entities.Identity;
+
+namespace PrecisionFarming.Infrastructure.DbContext
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed default roles
+            modelBuilder.Entity<AppRole>().HasData(
+                new AppRole
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new AppRole
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Farmer",
+                    NormalizedName = "FARMER"
+                },
+                new AppRole
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Agronomist",
+                    NormalizedName = "AGRONOMIST"
+                }
+            );
+        }
+    }
+}
