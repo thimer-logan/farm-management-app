a2a28e0130d3a9d4af950c23a2f2e5c3d27035e9
Logan Thimer <thimerlogan@gmail.com>
2025-02-26 06:58:12 -0700

    Backend - Added interface for FarmAccess repository

---
+++ backend/PrecisionFarming.Domain/Interfaces/Repositories/IFarmAccessRepository.cs
@@ -0,0 +1,12 @@
+﻿using PrecisionFarming.Domain.Entities;
+using PrecisionFarming.Domain.Enums;
+
+namespace PrecisionFarming.Domain.Interfaces.Repositories
+{
+    public interface IFarmAccessRepository : IRepository<FarmAccess>
+    {
+        Task<bool> ExistsAsync(Guid farmId, Guid userId);
+
+        Task<bool> HasAccessAsync(Guid farmId, Guid userId, AccessType accessType);
+    }
+}
